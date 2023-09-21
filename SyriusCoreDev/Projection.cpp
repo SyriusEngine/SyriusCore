61ae655b73504d8e912f23394d047c19185ecafb
artick788 <60552965+artick788@users.noreply.github.com>
2023-09-21 20:46:02 +0200

    added projection class

---
+++ SyriusCoreDev/Projection.cpp
@@ -0,0 +1,26 @@
+#include "Projection.hpp"
+
+Projection::Projection(ResourceView<Context> &context, uint32 width, uint32 height, float fov, float near, float far):
+m_Context(context){
+    m_ProjectionData.m_Perspective = glm::perspective(glm::radians(fov), (float)width / (float)height, near, far);
+    m_ProjectionData.m_Orthographic = glm::ortho(0.0f, (float)width, 0.0f, (float)height, near, far);
+
+    ConstantBufferDesc desc;
+    desc.shaderStage = SR_SHADER_VERTEX;
+    desc.size = sizeof(ProjectionData);
+    desc.type = SR_BUFFER_DYNAMIC;
+    desc.name = "ProjectionData";
+    desc.data = &m_ProjectionData;
+    desc.slot = 1;
+    m_Resource = m_Context->createConstantBuffer(desc);
+}
+
+
+Projection::~Projection() {
+
+}
+
+void Projection::bind() {
+    m_Resource->bind();
+}
+
---
+++ SyriusCoreDev/Projection.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Utils.hpp"
+
+struct ProjectionData{
+    glm::mat4 m_Perspective;
+    glm::mat4 m_Orthographic;
+};
+
+class Projection{
+public:
+    Projection(ResourceView<Context>& context, uint32 width, uint32 height, float fov = 45.0f, float near = 0.1f, float far = 100.0f);
+
+    ~Projection();
+
+    void bind();
+
+    void onResize(uint32 width, uint32 height);
+
+private:
+    ResourceView<Context>& m_Context;
+
+    ProjectionData m_ProjectionData;
+
+    ResourceView<ConstantBuffer> m_Resource;
+
+
+
+};
