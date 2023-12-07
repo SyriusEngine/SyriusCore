e59ed9fd8b09216d43ea856e4753bbf51d4bf85d
artick788 <60552965+artick788@users.noreply.github.com>
2023-12-07 15:08:03 +0100

    reworked Image library

--- main.cpp
+++ main.cpp
@@ -2,25 +2,8 @@
 #include "AppLayer.hpp"
 #include "TestLayers/BasicLayer.hpp"
 
-void test(){
-    ImageFileDesc desc;
-    desc.fileName = "./Resources/Textures/Texture1.jpg";
-    auto image = Syrius::createImage(desc);
-    printf("Image: %d %d\n", image->getWidth(), image->getHeight());
-    printf("ChannelCount: %d\n", image->getChannelCount());
-
-//    image->extendAlpha();
-    desc.fileName = "./Resources/Textures/Texture2.jpg";
-    desc.imgType = SR_IMAGE_JPG;
-    image->writeToFile(desc);
-
-}
-
-
 int main(int argc, char** argv) {
     try {
-        test();
-        return 0;
         for (int i = 1; i < argc; ++i) {
             SyriusCoreDev dev(argv[i]);
             dev.pushLayer<AppLayer>();
