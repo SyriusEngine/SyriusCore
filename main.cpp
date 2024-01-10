e27acc69f432e2febff89d1cae122f7917853947
artick788 <60552965+artick788@users.noreply.github.com>
2024-01-10 17:50:21 +0100

    push

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 #include "SyriusCoreDev/SyriusCoreDev.hpp"
 #include "AppLayer.hpp"
 
-void testImage(){
-    ImageFileDesc desc;
-    desc.fileName = "./Resources/Textures/HDR_040_Field/HDR_040_Field.hdr";
-    auto img = createImage(desc);
-
-    img->extendAlpha();
-
-    ImageFileDesc writeDesc;
-    writeDesc.fileName = "./Resources/Textures/HDR_040_Field/HDR_040_Field__2.hdr";
-    writeDesc.imgType = SR_IMAGE_HDR;
-    img->writeToFile(writeDesc);
-}
-
 int main(int argc, char** argv) {
     try {
-        testImage();
-        return 0;
         for (int i = 1; i < argc; ++i) {
             SyriusCoreDev dev(argv[i]);
             dev.pushLayer<AppLayer>();
